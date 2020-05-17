263b33446941328ee3c9e0ff61c76e28b23162af
Michael Lazar <mlazar@doctorondemand.com>
2020-05-17 15:00:51 -0400

    Formatting

--- jetforce/__init__.py
+++ jetforce/__init__.py
@@ -1,9 +1,10 @@
-# fmt: off
+"""
+isort:skip_file
+"""
 from .__version__ import __version__
-from .app.base import (JetforceApplication, Request, Response, RoutePattern,
-                       Status)
 from .protocol import GeminiProtocol
 from .server import GeminiServer
+from .app.base import JetforceApplication, Request, Response, RoutePattern, Status
 
 __title__ = "Jetforce Gemini Server"
 __author__ = "Michael Lazar"
