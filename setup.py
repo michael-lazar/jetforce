1cb5324a61c419a9593e969b8f83945ab3145f9a
Michael Lazar <mlazar@doctorondemand.com>
2020-05-23 21:31:41 -0400

    Forgot to remove old license classifier

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setuptools.setup(
     classifiers=[
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
